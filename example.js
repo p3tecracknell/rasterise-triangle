c2026c9d05888c3c3139fd2f4ded0b4a80ae1ec8
Pete Cracknell <p3tecracknell@gmail.com>
2018-06-22 23:56:58 +0100

    Add example.js

---
+++ example.js
@@ -0,0 +1,26 @@
+var fs = require('fs')
+var path = require('path')
+var Canvas = require('canvas')
+const rasterise = require('./rasterise')
+
+const width = 100
+const height = 100
+
+const canvas = Canvas.createCanvas(width, height)
+const ctx = canvas.getContext('2d')
+
+const imgData = ctx.getImageData(0, 0, width, height)
+const data = imgData.data
+
+var triangle = {
+  points: [
+    {x: 0, y: 0 },
+    {x: 50, y: 50 },
+    {x: 5, y: 50 }
+  ],
+  color: [255, 0, 0, 1]
+}
+
+rasterise.fillTriangle(triangle, data, width, height)
+ctx.putImageData(imgData, 0, 0)
+canvas.createPNGStream().pipe(fs.createWriteStream(path.join(__dirname, 'example.png')))
